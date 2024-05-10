917dae183d50c4b0bb851117574a0da0266d0064
RubyKyo <rubykyo@rk.vmfirewall.com>
2024-05-10 16:43:01 -0600

    rename C to C++

--- C/src/extra/scene.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../simengine.hpp"
-#include <vector>
-namespace Simengine {
-    struct Scene {
-        std::vector<Object> objects;
-    };
-};
--- C/src/simengine.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-namespace Simengine {
-    struct Object;
-    struct Scene;
-};
-#include "extra/scene.hpp"
-#include "extra/ecs/object.hpp"
